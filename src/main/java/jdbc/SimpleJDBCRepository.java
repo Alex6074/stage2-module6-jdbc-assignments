fa33f58615f46582cce0cdac5bb5ab487d5ba9b3
Aleksey Pardaev <lpardaev607@gmail.com>
2024-05-07 17:49:23 +0300

    9

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -20,35 +20,34 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    private static final String createUserSQL = "INSERT INTO myusers (id, firstname, lastname, age) VALUES (?, ?, ?, ?)";
-    private static final String updateUserSQL = "UPDATE myusers SET firstname = ?, lastname = ?, age = ? WHERE id = ?";
-    private static final String deleteUser = "DELETE FROM myusers WHERE id = ?";
-    private static final String findUserByIdSQL = "SELECT * FROM myusers WHERE id = ?";
-    private static final String findUserByNameSQL = "SELECT * FROM myusers WHERE firstname = ?";
-    private static final String findAllUserSQL = "SELECT * FROM myusers";
+    private static final String createUserSQL = "insert into myusers(firstname, lastname, age) values (?,?,?);";
+    private static final String updateUserSQL = "UPDATE myusers SET firstname = ?, lastname = ?, age = ? WHERE id = ?;";
+    private static final String deleteUser = "DELETE FROM myusers WHERE id = ?;";
+    private static final String findUserByIdSQL = "SELECT * FROM myusers WHERE id = ?;";
+    private static final String findUserByNameSQL = "SELECT * FROM myusers WHERE firstname = ?;";
+    private static final String findAllUserSQL = "SELECT * FROM myusers;";
 
     public Long createUser(User user) {
-        Long id = null;
+        Long userId = null;
         try {
             connection = CustomDataSource.getInstance().getConnection();
-            ps = connection.prepareStatement(createUserSQL, Statement.RETURN_GENERATED_KEYS);
-            ps.setLong(1, user.getId());
-            ps.setString(2, user.getFirstName());
-            ps.setString(3, user.getLastName());
-            ps.setInt(4, user.getAge());
-            ps.execute();
-            ResultSet resultSet = ps.getGeneratedKeys();
-            if (resultSet.next()) {
-                id =  resultSet.getLong(1);
+            if (connection != null) {
+                ps = connection.prepareStatement(createUserSQL, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, user.getFirstName());
+                ps.setString(2, user.getLastName());
+                ps.setInt(3, user.getAge());
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    userId = rs.getLong(1);
+                }
+                ps.close();
+                connection.close();
             }
-
-            ps.close();
-            connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-        return id;
+        return userId;
     }
 
     public User findUserById(Long userId) {
@@ -144,38 +143,5 @@ public class SimpleJDBCRepository {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static void main(String[] args) {
-        SimpleJDBCRepository repository = new SimpleJDBCRepository();
-
-        //User user = new User(4L, "dfkl", "Consdfdsnor", 23);
-        //System.out.println("createUser() method output: " + repository.createUser(user));
-
-        User user1 = repository.findUserById(8L);
-        System.out.println("findUserById() method output: " + user1);
-
-        User user3 = repository.findUserByName("Sarah");
-        System.out.println("findUserByName() method output: " + user3);
-
-//        List<User> userList = repository.findAllUser();
-//        System.out.println("findAllUser() method output: ");
-//        if (userList!=null) {
-//            for (User user: userList) {
-//                System.out.println(user.getId() + " " + user.getFirstName() + " " + user.getLastName() + " " + user.getAge());
-//            }
-//        }
-
-//        User user = new User(11L, "Josh", "Turner", 45);
-//        System.out.println("updateUser() method output:");
-//        if (repository.updateUser(user)!=null) {
-//            System.out.println("User: " + user.getId() + " " + user.getFirstName() + " " + user.getLastName() + " " + user.getAge());
-//            System.out.println("has been updated!");
-//        } else {
-//            System.out.println("User update failed!");
-//        }
-
-        //System.out.println("deleteUser() method execution");
-        //repository.deleteUser(6L);
     }
 }
--- src/main/java/jdbc/User.java
+++ src/main/java/jdbc/User.java
@@ -2,8 +2,6 @@ package jdbc;
 
 import lombok.*;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -12,4 +10,36 @@ public class User {
     private String firstName;
     private String lastName;
     private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
